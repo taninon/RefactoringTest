f98c4f38708e8f493360a118195a8ee2d0a29b78
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-22 10:11:02 +0900

    メソッドの抽出

---
+++ Assets/Script/TestMethodExtraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMethodExtraction : MonoBehaviour
+{
+	[SerializeField] private string name = "TestName";
+	[SerializeField] private int amount = 10;
+
+	private void Start()
+	{
+		List<int> testIntList = new List<int> { 2, 1, 5, 1, 5 };
+		foreach (var value in testIntList)
+		{
+			Debug.Log("List Value:" + value);
+		}
+
+		Debug.Log("name :" + name);
+		Debug.Log("amount :" + amount);
+	}
+}
