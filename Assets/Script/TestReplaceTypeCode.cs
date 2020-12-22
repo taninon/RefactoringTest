815148091998567ec7020221af9877c88b020099
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-22 12:18:13 +0900

    Switchを置き換える

---
+++ Assets/Script/TestReplaceTypeCode.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestReplaceTypeCode : MonoBehaviour
+{
+	private class Employee
+	{
+		private EmployeeType _type;
+
+		public EmployeeType Type => _type;
+
+		private int _monthlySalary;
+		private int _commission;
+		private int _bonus;
+
+		internal enum EmployeeType
+		{
+			Engineer,
+			Salesman,
+			Manager
+		}
+
+		private Employee(EmployeeType type)
+		{
+			_type = type;
+		}
+
+		public void SetSalary(int monthlySalary, int commission, int bonus)
+		{
+			_monthlySalary = monthlySalary;
+			_commission = commission;
+			_bonus = bonus;
+		}
+
+		public int PayAmount()
+		{
+			switch (_type)
+			{
+				case EmployeeType.Engineer:
+					return _monthlySalary;
+				case EmployeeType.Salesman:
+					return _monthlySalary + _commission;
+				case EmployeeType.Manager:
+					return _monthlySalary + _bonus;
+				default:
+					return 0;
+			}
+		}
+	}
+}
