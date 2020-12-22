2b36b01b5f25d46112137cc987e3a8833b98c8b4
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-22 10:42:03 +0900

    一時変数を減らす

---
+++ Assets/Script/TestReplaceTemp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestReplaceTemp : MonoBehaviour
+{
+	[SerializeField] private int quantity = 10;
+	[SerializeField] private int itemPrice = 200;
+
+	private void Start()
+	{
+		var price = GetPrice();
+		Debug.Log("価格:" + price);
+	}
+
+	private float GetPrice()
+	{
+		var basePrice = quantity * itemPrice;
+		if (basePrice > 1000)
+		{
+			return basePrice * 0.95f;
+		}
+		else
+		{
+			return basePrice * 0.98f;
+		}
+	}
+}
