77f0310e43758db705d8a3f6493369233c4c18b3
noriaki_tanimichi <tanimichi@morality-line.com>
2020-12-22 11:40:52 +0900

    パラメータ置き換え

---
+++ Assets/Script/TestRemoveAssignmentToPram.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRemoveAssignmentToPram : MonoBehaviour
+{
+	private void Start()
+	{
+		Debug.Log("代金" + 60);
+		Debug.Log("割引代金:" + Discount(60, 30, 8000));
+	}
+
+	private int Discount(int inputPrice, int quantity, int yearToDate)
+	{
+		if (inputPrice > 50)
+		{
+			inputPrice -= 5;
+		}
+
+		if (quantity > 100)
+		{
+			inputPrice -= 2;
+		}
+
+		if (yearToDate > 10000)
+		{
+			inputPrice -= 5;
+		}
+
+		return inputPrice;
+	}
+}
