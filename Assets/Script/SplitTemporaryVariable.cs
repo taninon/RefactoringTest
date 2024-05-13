4aafe0e49c667be78dc9a7baa21ac430acf58045
noriaki_tanimichi <tanimichi@morality-line.com>
2024-05-14 00:19:13 +0900

    SpllitTemporary

---
+++ Assets/Script/SplitTemporaryVariable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SplitTemporaryVariable : MonoBehaviour
+{
+	[SerializeField] private float speed = 5f;
+	[SerializeField] private Rigidbody rigidbody;
+
+	private void Update()
+	{
+		Vector3 velocity = new Vector3();
+		float velocityAxis;
+
+		velocityAxis = Input.GetAxis("Vertical");
+		velocity.y = velocityAxis;
+
+		velocityAxis = Input.GetAxis("Horizontal");
+		velocity.x = velocityAxis;
+
+		rigidbody.velocity = velocity * speed;
+	}
+}
