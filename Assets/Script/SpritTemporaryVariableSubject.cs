2da3af5d42b91195c5277a4557e92032338a8ea5
noriaki_tanimichi <tanimichi@morality-line.com>
2024-05-14 00:57:43 +0900

    Subject

---
+++ Assets/Script/SpritTemporaryVariableSubject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SpritTemporaryVariableSubject : MonoBehaviour
+{
+	[SerializeField] private float speed = 5f;
+	[SerializeField] private Rigidbody rigidbody;
+
+	void Update()
+	{
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		rigidbody.AddForce(x, 0, z, ForceMode.Impulse);
+	}
+}
