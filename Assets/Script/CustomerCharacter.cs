ea611194e09ed35120939ef00988614d9cce7db0
noriaki_tanimichi <tanimichi@morality-line.com>
2024-06-04 05:02:30 +0900

    Name

---
+++ Assets/Script/CustomerCharacter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CustomerCharacter : MonoBehaviour
+{
+	private string characterName;
+
+	public void SetName(string neme)
+	{
+		characterName = name;
+	}
+
+	public string GetName()
+	{
+		return characterName;
+	}
+
+}
