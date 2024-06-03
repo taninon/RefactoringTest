bb10d1b1b3e50c5f0bed3cfea080ce6490cfe155
noriaki_tanimichi <tanimichi@morality-line.com>
2024-06-04 04:20:28 +0900

    Add_ref004

---
+++ Assets/Script/Bumper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+	public float Bounce = 10f;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Ball")
+		{
+			collision.rigidbody.AddForce(0f, Bounce / 6, -Bounce, ForceMode.Impulse);
+		}
+	}
+}
---
+++ Assets/Script/PatrolEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+	[SerializeField] List<Transform> moveTargets;
+	[SerializeField] private CharacterController controller;
+	private int currentTargrtIndex = 0;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Start()
+	{
+		controller = this.gameObject.AddComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		transform.LookAt(moveTargets[currentTargrtIndex].transform);
+		moveDirection = moveTargets[currentTargrtIndex].transform.position - transform.position;
+		controller.SimpleMove(moveDirection * 0.5f);
+
+		if (Vector3.Distance(transform.position, moveTargets[currentTargrtIndex].transform.position) < 1.5f)
+		{
+			currentTargrtIndex = (currentTargrtIndex + 1) % moveTargets.Count;
+		}
+	}
+}
