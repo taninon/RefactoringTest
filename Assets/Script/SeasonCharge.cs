e6df3ee9f65175279aae01bfcc7f71bff09c6a02
noriaki_tanimichi <tanimichi@morality-line.com>
2024-06-18 03:07:40 +0900

    Season

---
+++ Assets/Script/SeasonCharge.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class SeasonCharge : MonoBehaviour
+{
+	private int month;
+
+	readonly int summerStartMonth = 6;
+	readonly int summerEndMonth = 8;
+
+	[SerializeField] int quantity;
+	[SerializeField] float winterRate;
+	[SerializeField] int winterServiceCharge;
+	[SerializeField] float summerRate;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		DateTime currentDate = DateTime.Now;
+		int month = currentDate.Month;
+		Debug.Log("¡‚Í" + month + "ŒŽ");
+
+		int charge = 0;
+		if (month >= summerStartMonth && month <= summerEndMonth)
+		{
+
+			charge = Mathf.CeilToInt(quantity * summerRate);
+		}
+		else
+		{
+			charge = Mathf.CeilToInt(quantity * winterRate) + winterServiceCharge;
+		}
+
+		Debug.Log("’l’i‚Í" + charge + "ŒŽ");
+	}
+}
